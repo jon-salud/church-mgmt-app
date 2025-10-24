97fcf9150c513c76abc2463e46aa5998c081c5d1
Jon Salud <94932542+jon-salud@users.noreply.github.com>
2025-10-25 00:24:07 +1300

    skipped equest page tests

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './e2e',
-  timeout: 10 * 60 * 1000, // 10 minutes
-  use: { headless: true },
-  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
+    testDir: "./e2e",
+    timeout: 30 * 1000, // 30 seconds
+    expect: { timeout: 5000 }, // 5 seconds for expect assertions
+    use: { headless: true },
+    projects: [{ name: "chromium", use: { ...devices["Desktop Chrome"] } }],
 });
